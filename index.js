5245a92660fb802c11f28671e302580c50537cac
Nilay Setiya <132999404+parthsetiya@users.noreply.github.com>
2023-06-14 20:05:10 +1200

    Added match loading from csv

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-
-var slideshows = document.querySelectorAll('[data-component="slideshow"]');
-slideshows.forEach(initSlideShow);
-
-function initSlideShow(slideshow) {
-
-	var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
-
-	var index = 0, time = 5000;
-	slides[index].classList.add('active');
-
-	setInterval( () => {
-		slides[index].classList.remove('active');
-		
-		index++;
-		if (index === slides.length) index = 0;
-
-		slides[index].classList.add('active');
-
-	}, time);
-}
+Papa.parse("./UpcomingMatches.csv", {
+  download: true,
+  header: true,
+  complete: function (results) {
+    const today = new Date();
+    console.log(results.data);
+    today.setHours(0, 0, 0, 0); // To ignore time portion for comparison
+    const matches = results.data.filter((match) => {
+      if (!match.DateOfMatch) {
+        return false;
+      }
+      let dateString = match.DateOfMatch;
+      let parts = dateString.split("/");
+      let formattedDate = new Date(`${parts[2]}-${parts[1]}-${parts[0]}`);
+      const matchDate = new Date(formattedDate);
+      return matchDate >= today;
+    });
+    if (matches.length > 0) {
+      const nextMatch = matches[0]; // Assuming the data is sorted by date
+      document.getElementById("team1-img").src = nextMatch["Team1ImgSrc"];
+      document.getElementById("team2-img").src = nextMatch["Team2ImgSrc"];
+      document.getElementById("team1-name").innerText = nextMatch.Team1;
+      document.getElementById("team2-name").innerText = nextMatch.Team2;
+      document.getElementById("match-date").innerText = nextMatch.DateOfMatch;
+    }
+  },
+});
